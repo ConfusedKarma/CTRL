343793d87032e2ac02f55c6c524c4a7d2e0beed5
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-03-01 18:18:03 +0530

    Update misc.py

--- tg_bot/modules/helper_funcs/misc.py
+++ tg_bot/modules/helper_funcs/misc.py
@@ -133,3 +133,12 @@ def build_keyboard_parser(bot, chat_id, buttons):
             keyb.append([InlineKeyboardButton(btn.name, url=btn.url)])
 
     return keyb
+
+
+
+def delete(delmsg, timer):
+    sleep(timer)
+    try:
+        delmsg.delete()
+    except:
+        return
