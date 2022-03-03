119ce9b00cf126ea3b1325e7e270d3ce2b22452e
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-03-03 18:02:58 +0530

    Update gtranslator.py

--- tg_bot/modules/gtranslator.py
+++ tg_bot/modules/gtranslator.py
@@ -1,162 +1,68 @@
-from typing import Optional, List
-
-from telegram import Message, Update, Bot, User
-from telegram import MessageEntity
-from telegram.ext import Filters, MessageHandler, run_async
-from telegram import ParseMode
-
-from tg_bot import dispatcher, LOGGER
+from gpytranslate import SyncTranslator
+from telegram import ParseMode, Update, Bot
+from tg_bot import dispatcher
 from tg_bot.modules.disable import DisableAbleCommandHandler
-from emoji import UNICODE_EMOJI
 
-from googletrans import Translator
-
-
-@run_async
-def translate(bot: Bot, update: Update):
-	msg = update.effective_message
-	chat_id = update.effective_chat.id
-	"""
-	lang = str("af,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,et,eu,fa,fi,fr,fy,ga,gd,gl,gu,ha,haw,hi,hmn,hr,ht,hu,hy,id,ig,is,it,iw,ja,jw,ka,kk,km,kn,ko,ku,ky,la,lb,lo,lt,lv,mg,mi,mk,ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,vi,xh,yi,yo,zh,zh_CN,zh_TW,zu".split(","))
-	try:
-		if msg.reply_to_message and msg.reply_to_message.text:
-			args = update.effective_message.text.split(None, 1)
-			try:
-				target = args[1].split(None, 1)[0]
-			except:
-				target = args[1]
-			try:
-				target2 = args[1].split(None, 2)[1]
-				if target2 not in lang:
-					target2 = None
-				else:
-					target = args[1].split(None, 2)[0]
-					target2 = args[1].split(None, 2)[1]
-			except:
-				target2 = None
-			text = msg.reply_to_message.text
-			message = update.effective_message
-			trl = Translator()
-			if target2 == None:
-				detection = trl.detect(text)
-				tekstr = trl.translate(text, dest=target)
-				if tekstr.pronunciation == None:
-					return message.reply_text("Translated from `{}` to `{}`:\n`{}`".format(detection.lang, target, tekstr.text), parse_mode=ParseMode.MARKDOWN)
-				else:
-					return message.reply_text("Translated from `{}` to `{}`:\n*Characters:*\n`{}`\n*Text:*\n`{}`".format(detection.lang, target, tekstr.text, tekstr.pronunciation), parse_mode=ParseMode.MARKDOWN)
-			else:
-				tekstr = trl.translate(text, dest=target2, src=target)
-				if tekstr.pronunciation == None:
-					return message.reply_text("Translated from `{}` to `{}`:\n`{}`".format(target, target2, tekstr.text), parse_mode=ParseMode.MARKDOWN)
-				else:
-					return message.reply_text("Translated from `{}` to `{}`:\n*Characters:*\n`{}`\n*Text:*\n`{}`".format(target, target2, tekstr.text, tekstr.pronunciation), parse_mode=ParseMode.MARKDOWN)
-			
-		else:
-			args = update.effective_message.text.split(None, 2)
-			target = args[1]
-			try:
-				target2 = args[2].split(None, 1)[0]
-				if target2 not in lang:
-					target2 = None
-					text = args[2]
-				else:
-					target2 = args[2].split(None, 1)[0]
-					text = args[2].split(None, 1)[1]
-			except:
-				target2 = None
-				text = args[2]
-			message = update.effective_message
-			trl = Translator()
-			if target2 == None:
-				detection = trl.detect(text)
-				tekstr = trl.translate(text, dest=target)
-				if tekstr.pronunciation == None:
-					return message.reply_text("Translated from `{}` to `{}`:\n`{}`".format(detection.lang, target, tekstr.text), parse_mode=ParseMode.MARKDOWN)
-				else:
-					return message.reply_text("Translated from `{}` to `{}`:\n*Characters:*\n`{}`\n*Text:*\n`{}`".format(detection.lang, target, tekstr.text, tekstr.pronunciation), parse_mode=ParseMode.MARKDOWN)
-			else:
-				tekstr = trl.translate(text, dest=target2, src=target)
-				if tekstr.pronunciation == None:
-					return message.reply_text("Translated from `{}` to `{}`:\n`{}`".format(target, target2, tekstr.text), parse_mode=ParseMode.MARKDOWN)
-				else:
-					return message.reply_text("Translated from `{}` to `{}`:\n*Characters:*\n`{}`\n*Text:*\n`{}`".format(target, target2, tekstr.text, tekstr.pronunciation), parse_mode=ParseMode.MARKDOWN)
-	"""
-	try:
-		if msg.reply_to_message and msg.reply_to_message.text:
-			args = update.effective_message.text.split(None, 1)
-			target2 = None
-			try:
-				target = args[1].split(None, 1)[0]
-			except:
-				target = args[1]
-			if "-" in target:
-				target2 = target.split("-")[1]
-				target = target.split("-")[0]
-			text = msg.reply_to_message.text
-			#text = deEmojify(text)
-			exclude_list = UNICODE_EMOJI.keys()
-			for emoji in exclude_list:
-				if emoji in text:
-					text = text.replace(emoji, '')
-			message = update.effective_message
-			trl = Translator()
-			if target2 is None:
-				detection = trl.detect(text)
-				tekstr = trl.translate(text, dest=target)
-				if tekstr.pronunciation is None:
-					return message.reply_text("Translated from `{}` to `{}`:\n`{}`".format(detection.lang, target, tekstr.text), parse_mode=ParseMode.MARKDOWN)
-				else:
-					return message.reply_text("Translated from `{}` to `{}`:\n`{}`".format(detection.lang, target, tekstr.pronunciation), parse_mode=ParseMode.MARKDOWN)
-			else:
-				tekstr = trl.translate(text, dest=target2, src=target)
-				if tekstr.pronunciation is None:
-					return message.reply_text("Translated from `{}` to `{}`:\n`{}`".format(target, target2, tekstr.text), parse_mode=ParseMode.MARKDOWN)
-				else:
-					return message.reply_text("Translated from `{}` to `{}`:\n`{}`".format(target, target2, tekstr.pronunciation), parse_mode=ParseMode.MARKDOWN)
-
-		else:
-			args = update.effective_message.text.split(None, 2)
-			target = args[1]
-			text = args[2]
-			#text = deEmojify(text)
-			exclude_list = UNICODE_EMOJI.keys()
-			for emoji in exclude_list:
-				if emoji in text:
-					text = text.replace(emoji, '')
-			target2 = None
-			if "-" in target:
-				target2 = target.split("-")[1]
-				target = target.split("-")[0]
-			message = update.effective_message
-			trl = Translator()
-			if target2 is None:
-				detection = trl.detect(text)
-				tekstr = trl.translate(text, dest=target)
-				return message.reply_text("Translated from `{}` to `{}`:\n`{}`".format(detection.lang, target, tekstr.text), parse_mode=ParseMode.MARKDOWN)
-				#if tekstr.pronunciation == None:
-				#	return message.reply_text("Translated from `{}` to `{}`:\n`{}`".format(detection.lang, target, tekstr.text), parse_mode=ParseMode.MARKDOWN)
-				#else:
-				#	return message.reply_text("Translated from `{}` to `{}`:\n`{}`".format(detection.lang, target, tekstr.pronunciation), parse_mode=ParseMode.MARKDOWN)
-			else:
-				tekstr = trl.translate(text, dest=target2, src=target)
-				message.reply_text("Translated from `{}` to `{}`:\n`{}`".format(target, target2, tekstr.text), parse_mode=ParseMode.MARKDOWN)
-				#if tekstr.pronunciation == None:
-				#	return message.reply_text("Translated from `{}` to `{}`:\n`{}`".format(target, target2, tekstr.text), parse_mode=ParseMode.MARKDOWN)
-				#else:
-				#	return message.reply_text("Translated from `{}` to `{}`:\n`{}`".format(target, target2, tekstr.pronunciation), parse_mode=ParseMode.MARKDOWN)
-
-	except IndexError:
-		update.effective_message.reply_text("Reply to messages or write messages from other languages ​​for translating into the intended language\n\nExample: `/tr en-ml` to translate from English to Malayalam\nOr use: `/tr ml` for automatic detection and translating it into Malayalam", parse_mode="markdown")
-	except ValueError:
-		update.effective_message.reply_text("The intended language is not found!")
-	else:
-		return
+trans = SyncTranslator()
 
 
-__help__ = """- /tr (language code) as reply to a long message.
-"""
-__mod_name__ = "Translator"
+def translate(bot: Bot, update: Update) -> None:
+    message = update.effective_message
+    reply_msg = message.reply_to_message
+    if not reply_msg:
+        message.reply_text("Reply to a message to translate it!")
+        return
+    if reply_msg.caption:
+        to_translate = reply_msg.caption
+    elif reply_msg.text:
+        to_translate = reply_msg.text
+    try:
+        args = message.text.split()[1].lower()
+        if "//" in args:
+            source = args.split("//")[0]
+            dest = args.split("//")[1]
+        else:
+            source = trans.detect(to_translate)
+            dest = args
+    except IndexError:
+        source = trans.detect(to_translate)
+        dest = "en"
+    translation = trans(to_translate,
+                        sourcelang=source, targetlang=dest)
+    reply = f"<b>Translated from {source} to {dest}</b>:\n" \
+        f"<code>{translation.text}</code>"
 
-TRANSLATE_HANDLER = DisableAbleCommandHandler("tr", translate)
+    message.reply_text(reply, parse_mode=ParseMode.HTML)
 
+
+
+def languages(bot: Bot, update: Update) -> None:
+    message = update.effective_message
+    message.reply_text(
+        "Click [here](https://cloud.google.com/translate/docs/languages)"
+        " to see the list of supported language codes!",
+        disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
+
+
+__mod_name__ = "Translation"
+
+__help__ = """
+Use this module to translate stuff... duh!
+
+*Commands:*
+• `/tl` (or `/tr`): as a reply to a message, translates it to English.
+
+• `/tl <lang>`: translates to <lang>
+eg: `/tl ja`: translates to Japanese.
+
+• `/tl <source>//<dest>`: translates from <source> to <lang>.
+eg: `/tl ja//en`: translates from Japanese to English.
+
+• `/langs`: get a list of supported languages for translation."""
+
+
+
+TRANSLATE_HANDLER = DisableAbleCommandHandler(["tl", "tr"], translate)
+LANGUAGES_HANDLER = DisableAbleCommandHandler("langs", languages)
 dispatcher.add_handler(TRANSLATE_HANDLER)
+dispatcher.add_handler(LANGUAGES_HANDLER)
