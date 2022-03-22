88a2e46948f641a1b6438503e005a3ea40c78af9
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-03-22 22:45:25 +0530

    Update connection_sql.py

--- tg_bot/modules/sql/connection_sql.py
+++ tg_bot/modules/sql/connection_sql.py
@@ -34,7 +34,7 @@ class Connection(BASE):
 
 class ConnectionHistory(BASE):
     __tablename__ = "connection_history"
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)
     chat_id = Column(String(14), primary_key=True)
     chat_name = Column(UnicodeText)
     conn_time = Column(Integer)
