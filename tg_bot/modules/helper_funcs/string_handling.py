772549f89ec21622b5f47251346598e245320b68
Sourcery AI <>
2022-08-18 05:28:36 +0000

    'Refactored by Sourcery'

--- tg_bot/modules/helper_funcs/string_handling.py
+++ tg_bot/modules/helper_funcs/string_handling.py
@@ -81,35 +81,30 @@ def markdown_parser(txt: str, entities: Dict[MessageEntity, str] = None, offset:
         start = ent.offset + offset  # start of entity
         end = ent.offset + offset + ent.length - 1  # end of entity
 
-        # we only care about code, url, text links
-        if ent.type in ("code", "url", "text_link"):
-            # count emoji to switch counter
-            count = _calc_emoji_offset(txt[:start])
-            start -= count
-            end -= count
+        if ent.type not in ("code", "url", "text_link"):
+            continue
+
+        # count emoji to switch counter
+        count = _calc_emoji_offset(txt[:start])
+        start -= count
+        end -= count
 
             # URL handling -> do not escape if in [](), escape otherwise.
-            if ent.type == "url":
-                if any(match.start(1) <= start and end <= match.end(1) for match in LINK_REGEX.finditer(txt)):
-                    continue
-                # else, check the escapes between the prev and last and forcefully escape the url to avoid mangling
-                else:
-                    # TODO: investigate possible offset bug when lots of emoji are present
-                    res += _selective_escape(txt[prev:start] or "") + escape_markdown(ent_text)
+        if ent.type == "url":
+            if any(match.start(1) <= start and end <= match.end(1) for match in LINK_REGEX.finditer(txt)):
+                continue
+            # else, check the escapes between the prev and last and forcefully escape the url to avoid mangling
+            else:
+                # TODO: investigate possible offset bug when lots of emoji are present
+                res += _selective_escape(txt[prev:start] or "") + escape_markdown(ent_text)
 
-            # code handling
-            elif ent.type == "code":
-                res += _selective_escape(txt[prev:start]) + '`' + ent_text + '`'
+        elif ent.type == "code":
+            res += f'{_selective_escape(txt[prev:start])}`{ent_text}`'
 
-            # handle markdown/html links
-            elif ent.type == "text_link":
-                res += _selective_escape(txt[prev:start]) + "[{}]({})".format(ent_text, ent.url)
+        elif ent.type == "text_link":
+            res += _selective_escape(txt[prev:start]) + f"[{ent_text}]({ent.url})"
 
-            end += 1
-
-        # anything else
-        else:
-            continue
+        end += 1
 
         prev = end
 
@@ -140,8 +135,7 @@ def button_markdown_parser(txt: str, entities: Dict[MessageEntity, str] = None, 
         else:
             note_data += markdown_note[prev:to_check]
             prev = match.start(1) - 1
-    else:
-        note_data += markdown_note[prev:]
+    note_data += markdown_note[prev:]
 
     return note_data, buttons
 
@@ -253,18 +247,21 @@ def extract_time(message, time_val):
             return ""
         return bantime
     else:
-        message.reply_text("Invalid time type specified. Expected m,h, or d, got: {}".format(time_val[-1]))
+        message.reply_text(
+            f"Invalid time type specified. Expected m,h, or d, got: {time_val[-1]}"
+        )
+
         return ""
 
 def make_time(time_val):
     if int(time_val) == 0:
         return "0"
     if int(time_val) <= 3600:
-        bantime = str(int(time_val / 60)) + "m"
+        bantime = f"{int(time_val / 60)}m"
     elif int(time_val) >= 3600 and time_val <= 86400:
-        bantime = str(int(time_val / 60 / 60)) + "h"
+        bantime = f"{int(time_val / 60 / 60)}h"
     elif int(time_val) >= 86400:
-        bantime = str(int(time_val / 24 / 60 / 60)) + "d"
+        bantime = f"{int(time_val / 24 / 60 / 60)}d"
     return bantime
 
 def markdown_to_html(text):
